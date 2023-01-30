9c0894ff68819168581589d7e1daf298803a85ae
Dhiraj Kapse <77230041+dhir4j@users.noreply.github.com>
2023-01-30 16:56:34 +0530

    Updated bot.sendmessage
    Now bot replies to person who sent the command

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def generate_reply(message):
     try:
         if message.chat.type == "private":
             chat_id = message.from_user.id
-            bot.send_message(chat_id=chat_id, text="Use this command in group and Use it as a reply for another perosn's message.")
+            bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text="Use this command in a group and reply to a message.")
             return
         reply_to_message = message.reply_to_message # get the message that the '/reply' command is replying to
         prompt = reply_to_message.text
@@ -22,9 +22,9 @@ def generate_reply(message):
             prompt=prompt,
             max_tokens=2048
         )
-        bot.send_message(chat_id=message.chat.id, text=response["choices"][0]["text"])
+        bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=response["choices"][0]["text"])
     except Exception as e:
-        bot.send_message(chat_id=message.chat.id, text="Use it as a reply for another perosn's message. Use /gpt instead for your personal message".format(str(e)))
+        bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text="Use this command in a group and reply to a message.")
 
 @bot.message_handler(commands=['gpt'])
 def generate_gpt(message):
@@ -36,15 +36,15 @@ def generate_gpt(message):
         chat_id = message.from_user.id
     prompt_array = message.text.split()[1:]
     if len(prompt_array) < 2:
-      bot.send_message(chat_id=chat_id, text="Please provide a message after the command '/gpt'")
-      return
-    prompt = message.text.split()[1:] # get the text after the command '/reply'
+        bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text="Please provide a message after the command '/gpt'")
+        return
+    prompt = message.text.split()[1:] # get the text after the command '/gpt'
     prompt = ' '.join(prompt)
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         max_tokens=2048
     )
-    bot.send_message(chat_id=message.chat.id, text=response["choices"][0]["text"])
+    bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=response["choices"][0]["text"])
 
 bot.polling()
