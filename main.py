a52d5ae50eeaf402506f20a996622ddd61756934
Dhiraj Kapse <77230041+dhir4j@users.noreply.github.com>
2023-01-31 19:49:45 +0530

    Added reply wiith audio /gpta feature
    removed /roast /replytg and added /gpta

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ openai.api_key = " enter open ai api key here "
 
 bot = telebot.TeleBot(' enter telegram token here ')
 
+
 @bot.message_handler(commands=['reply'])
 def generate_reply(message):
     try:
@@ -69,38 +70,6 @@ def generate_roastreply(message):
             print(e)
 
 
-@bot.message_handler(commands=['replytg'])
-def generate_tgreply(message):
-    try:
-        if message.chat.type == "private":
-            chat_id = message.from_user.id
-            bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text="Use this command in a group and reply to a message.")
-            return
-        reply_to_message = message.reply_to_message
-        prompt = reply_to_message.text
-        if len(message.text.split()) > 1:
-            prompt = prompt + " " + " ".join(message.text.split()[1:])
-        prompt = prompt + " This is a telegram message sent by a person write a reply for it"
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=2048
-        )
-        if len(response["choices"][0]["text"]) > 3900:
-            bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text="Message is too big, Telegram doesn't support")
-        else:
-            if reply_to_message.from_user.is_bot:
-                bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,text=response["choices"][0]["text"])
-            else:
-                bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=response["choices"][0]["text"])
-        print(f'\n\n=======================================================================================\n{message.from_user.username} : {prompt} \n\nChatGPT : {response["choices"][0]["text"]}')
-    except Exception:
-        try:
-            bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text="Reply to a message")
-        except Exception as e:
-            print(e)
-
-
 @bot.message_handler(commands=['gpt'])
 def generate_gpt(message):
     try:
@@ -116,39 +85,6 @@ def generate_gpt(message):
             return
         prompt = message.text.split()[1:] # get the text after the command '/gpt'
         prompt = ' '.join(prompt)
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=2048
-        )
-        response_text = response["choices"][0]["text"]
-        if len(response_text) > 3900:
-            raise Exception("Message is too big, Telegram doesn't support messages with more than 3900 characters.")
-        bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=response_text)
-        print(f'\n\n=======================================================================================\n{message.from_user.username} : {prompt} \n\nChatGPT : {response["choices"][0]["text"]}')
-    except Exception as e:
-        try:
-            bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=str(e))
-        except Exception as e:
-            print(e)
-
-
-@bot.message_handler(commands=['roast'])
-def generate_roastgpt(message):
-    try:
-        if message.chat.type != "private":
-        # message was sent in a group, respond to the group
-            chat_id = message.chat.id
-        else:
-        # message was sent in private chat, respond to the sender
-            chat_id = message.from_user.id
-        prompt_array = message.text.split()[1:]
-        if len(prompt_array) < 2:
-            bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text="Please provide a message after the command '/roast'")
-            return
-        prompt = message.text.split()[1:] # get the text after the command '/gpt'
-        prompt = ' '.join(prompt)
-        prompt = "roast this : "+prompt
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
@@ -196,6 +132,47 @@ def generate_trengpt(message):
         if len(response_text) > 3900:
             raise Exception("Message is too big, Telegram doesn't support messages with more than 3900 characters.")
         bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=response_text)
+        print(f'\n\n=======================================================================================\n{message.from_user.username} : {prompt} \n\nChatGPT : {response["choices"][0]["text"]}')
+    except Exception as e:
+        try:
+            bot.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id, text=str(e))
+        except Exception as e:
+            print(e)
+
+@bot.message_handler(commands=['gpta'])
+def generate_gptaudio(message):
+    try:
+        if message.chat.type != "private":
+        # message was sent in a group, respond to the group
+            chat_id = message.chat.id
+        else:
+        # message was sent in private chat, respond to the sender
+            chat_id = message.from_user.id
+        prompt_array = message.text.split()[1:]
+        if len(prompt_array) < 2:
+            bot.send_message(chat_id=chat_id, reply_to_message_id=message.message_id, text="Please provide a message after the command '/audio'")
+            return
+        prompt = message.text.split()[1:] # get the text after the command '/audio'
+        prompt = ' '.join(prompt)
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=2048
+        )
+        response_text = response["choices"][0]["text"]
+        if len(response_text) > 3900:
+            raise Exception("Message is too big, Telegram doesn't support messages with more than 3900 characters.")
+
+        # Convert the text to audio using gTTS library
+        from gtts import gTTS
+        tts = gTTS(response_text, lang='en')
+        tts.save("dev_@dhir4j.mp3")
+
+        # Send the audio file
+        with open("dev_@dhir4j.mp3", "rb") as f:
+            bot.send_audio(chat_id=chat_id, reply_to_message_id=message.message_id ,audio=f)
+
+        os.remove("dev_@dhir4j.mp3") # Delete the audio file
         print(f'\n\n=======================================================================================\n{message.from_user.username} : {prompt} \n\nChatGPT : {response["choices"][0]["text"]}')
     except Exception as e:
         try:
